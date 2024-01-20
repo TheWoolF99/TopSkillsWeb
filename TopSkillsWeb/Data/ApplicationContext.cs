b3028b0cea2b17cb661548d03cdbd9811c46bf02
TheWoolF99 <maxcleviu@gmail.com>
2024-01-20 18:13:58 +0300

    Добавьте файлы проекта.

---
+++ TopSkillsWeb/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TopSkillsWeb.Models;
+
+namespace TopSkillsWeb.Data
+{
+    public class ApplicationContext: IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+           : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public User User { get; set; }
+    }
+}
---
+++ TopSkillsWeb/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TopSkillsWeb.Models;
+using TopSkillsWeb.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+string ConnString = "Prodaction";
+
+builder.Services.AddDbContext<ApplicationContext>(
+                options =>
+                {
+                    options.UseSqlServer(builder.Configuration.GetConnectionString(ConnString));
+                } 
+                , ServiceLifetime.Transient
+                );
+builder.Services.AddIdentity<User, IdentityRole>(opts =>
+{
+    opts.Password.RequiredLength = 4;   // минимальная длина
+    opts.Password.RequireNonAlphanumeric = false;   // требуются ли не алфавитно-цифровые символы
+    opts.Password.RequireLowercase = false; // требуются ли символы в нижнем регистре
+    opts.Password.RequireUppercase = false; // требуются ли символы в верхнем регистре
+    opts.Password.RequireDigit = false; // требуются ли цифры
+}).AddEntityFrameworkStores<ApplicationContext>().AddDefaultTokenProviders();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
